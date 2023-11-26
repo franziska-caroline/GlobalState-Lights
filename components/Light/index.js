4439a2205b992ed82fb9fa51142ce8a8944497d0
Franziska Adam <franziskacarolineadam@gmail.com>
2023-11-26 13:49:11 +0100

    Complete Task1

--- components/Light/index.js
+++ components/Light/index.js
@@ -1,24 +1,12 @@
 import { useState } from "react";
 import { LightButton, Icon, Text, Name, State } from "./Light.styled";
 
-export default function Light({ name }) {
-  const [isOn, setIsOn] = useState(false);
-
-  function handleToggle() {
-    setIsOn((isOn) => !isOn);
-  }
-
+export default function Light({ light, isOn, onToggleLight }) {
   return (
-    <LightButton
-      type="button"
-      onClick={() => {
-        handleToggle();
-      }}
-      $isOn={isOn}
-    >
+    <LightButton type="button" onClick={onToggleLight} $isOn={isOn}>
       <Icon $isOn={isOn}>💡</Icon>
       <Text>
-        <Name>{name}</Name>
+        <Name>{light.name}</Name>
         <State>{isOn ? "On" : "Off"}</State>
       </Text>
     </LightButton>
